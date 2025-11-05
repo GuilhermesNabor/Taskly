31ae5e829935558e8a793bc3c7a0dc5765ec835a
GuilhermesNabor <guilhermenabor@outlook.com>
2025-11-05 20:37:48 -0300

    feat: improve .gitignore and generate apk

--- android/app/src/main/java/com/taskly/MainApplication.kt
+++ android/app/src/main/java/com/taskly/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.meuapp
+package com.taskly
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/Taskly/AppDelegate.swift
+++ ios/Taskly/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "MeuApp",
+      withModuleName: "Taskly",
       in: window,
       launchOptions: launchOptions
     )
